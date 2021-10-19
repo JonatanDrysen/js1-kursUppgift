7759a2efa072050df8c1ca822e1aa60931958474
Jonatan Drysén <jojodrysen@gmail.com>
2021-10-19 17:45:27 +0200

    init

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const url = 'https://mock-data-api.firebaseio.com/webb21/products.json';
-const productsContainerElement = document.getElementById('products-container');
-const totalSpent = document.getElementById('total-spent');
+const url = "https://mock-data-api.firebaseio.com/webb21/products.json";
+const productsContainerElement = document.getElementById("products-container");
+const totalSpent = document.getElementById("total-spent");
 let sum = 0;
 
 function renderProductList(productData) {
@@ -10,7 +10,7 @@ function renderProductList(productData) {
 }
 
 function renderProductItem(productItem) {
-  const productItemElement = document.createElement('div');
+  const productItemElement = document.createElement("div");
 
   productItemElement.append(
     renderItemName(productItem),
@@ -25,16 +25,16 @@ function renderProductItem(productItem) {
 }
 
 function renderItemName(productItem) {
-  const productNameElement = document.createElement('h1');
+  const productNameElement = document.createElement("h1");
   productNameElement.innerText = productItem.name;
   return productNameElement;
 }
 
 function renderItemImage(productItem) {
-  const productImageElement = document.createElement('img');
+  const productImageElement = document.createElement("img");
   productImageElement.src = productItem.images[0].src.small;
   productImageElement.alt = productItem.images[0].alt;
-  productImageElement.addEventListener('click', function () {
+  productImageElement.addEventListener("click", function () {
     buyItem(productItem);
   });
 
@@ -42,25 +42,25 @@ function renderItemImage(productItem) {
 }
 
 function renderItemDescription(productItem) {
-  const productDescriptionElement = document.createElement('p');
+  const productDescriptionElement = document.createElement("p");
   productDescriptionElement.innerText = productItem.description;
   return productDescriptionElement;
 }
 
 function renderItemPrice(productItem) {
-  const productPriceElement = document.createElement('p');
+  const productPriceElement = document.createElement("p");
   productPriceElement.innerText = `Price: ${productItem.price} dabloons`;
   return productPriceElement;
 }
 
 function renderItemRating(productItem) {
-  const productRatingElement = document.createElement('p');
+  const productRatingElement = document.createElement("p");
   productRatingElement.innerText = `Rating: ${productItem.rating} stars`;
   return productRatingElement;
 }
 
 function renderItemStock(productItem) {
-  const productStockElement = document.createElement('p');
+  const productStockElement = document.createElement("p");
   productStockElement.innerText = `Stock: ${productItem.stock} in stock`;
   return productStockElement;
 }
